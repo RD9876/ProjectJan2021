73969536d059f4ebd437e404cae4ce75dbf9934a
RD9876 <renudev2706@gmail.com>
2021-01-09 11:31:25 +0530

    addedc to A

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -4,5 +4,8 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Program");
 		System.out.println("Java");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 	}
 }
